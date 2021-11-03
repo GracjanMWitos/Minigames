13a1cfad636c4d5de7864a61a68c3a15266e9e72
GracjanMWitos <gracjan-witos@wp.pl>
2021-11-04 00:28:37 +0100

    Update #1

--- OX/Assets/Scripts/GameController.cs
+++ OX/Assets/Scripts/GameController.cs
@@ -40,12 +40,40 @@ public class GameController : MonoBehaviour
             }
         }
         else highlightTransform.position = new Vector3(10f, 10f, 0f);
+        ResetSigns();
     }
     void FindWinner()
     {
-        if ((fields[0].fieldWithO && fields[1].fieldWithO && fields[2].fieldWithO))
+        if ((fields[0].fieldWithO && fields[1].fieldWithO && fields[2].fieldWithO) || (fields[3].fieldWithO && fields[4].fieldWithO && fields[5].fieldWithO) 
+            || (fields[6].fieldWithO && fields[7].fieldWithO && fields[8].fieldWithO) || (fields[0].fieldWithO && fields[3].fieldWithO && fields[6].fieldWithO)
+            || (fields[1].fieldWithO && fields[4].fieldWithO && fields[7].fieldWithO) || (fields[2].fieldWithO && fields[5].fieldWithO && fields[8].fieldWithO)
+            || (fields[0].fieldWithO && fields[4].fieldWithO && fields[8].fieldWithO) || (fields[2].fieldWithO && fields[4].fieldWithO && fields[6].fieldWithO))
         {
             print("O");
+        }
+        if ((fields[0].fieldWithX && fields[1].fieldWithX && fields[2].fieldWithX) || (fields[3].fieldWithX && fields[4].fieldWithX && fields[5].fieldWithX)
+    || (fields[6].fieldWithX && fields[7].fieldWithX && fields[8].fieldWithX) || (fields[0].fieldWithX && fields[3].fieldWithX && fields[6].fieldWithX)
+    || (fields[1].fieldWithX && fields[4].fieldWithX && fields[7].fieldWithX) || (fields[2].fieldWithX && fields[5].fieldWithX && fields[8].fieldWithX)
+    || (fields[0].fieldWithX && fields[4].fieldWithX && fields[8].fieldWithX) || (fields[2].fieldWithX && fields[4].fieldWithX && fields[6].fieldWithX))
+        {
+            print("X");
+        }
+    }
+   private void ResetSigns()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GameObject[] signs = GameObject.FindGameObjectsWithTag("Sign");
+            foreach (GameObject sign in signs)
+                Destroy(sign);
+
+            foreach (Field field in fields)
+            {
+                field.fieldWithX = false;
+                field.fieldWithO = false;
+            }
+
+            change = false;
         }
     }
 }
